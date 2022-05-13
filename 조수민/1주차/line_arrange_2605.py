56a33967fb1e23e95f9ab394a1bbd0d0e18c31aa
Jasmin0010 <sumichip4035@naver.com>
2022-05-13 12:15:04 +0900

    [2605] line arrange

---
+++ 조수민/1주차/line_arrange_2605.py
@@ -0,0 +1,12 @@
+# Week 1_No.2605 : line arrange
+
+stu_num = int(input())
+num_pick = list(map(int, input().split(" ")))
+
+result = []
+
+for i in range(N):
+    result.insert(i-num_pick[i], i+1)
+
+for k in result:
+    print(k, end=' ')
