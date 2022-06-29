3c50259c882147b785b756d29ab8832f02dbb3c5
Jasmin0010 <sumichip4035@naver.com>
2022-06-29 18:41:27 +0900

    2주차-[그리디] 1이 될 때까지

---
+++ 조수민/2주차/tilone.py
@@ -0,0 +1,16 @@
+N, K = map(int,input().split( ))
+
+try_num = 0
+
+while N>1:
+    if N%K==0:
+        N = N/K
+        try_num +=1
+    else:
+        N -=1
+        try_num +=1
+
+print(try_num)
+
+
+
