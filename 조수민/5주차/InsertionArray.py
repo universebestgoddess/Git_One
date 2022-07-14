b66da56639b990a2e9150a8fbf15502e120b3d9b
Jasmin0010 <sumichip4035@naver.com>
2022-07-14 17:08:00 +0900

    [5주차] 삽입정렬

---
+++ 조수민/5주차/InsertionArray.py
@@ -0,0 +1,12 @@
+from socket import J1939_FILTER_MAX
+
+
+array = [7,5,9,0,3,1,6,2,4,8]
+
+for i in range(1, len(array)):
+    for j in range(i,0,-1):
+        if array[j] < array[j-1]:
+            array[j], array[j-1] = array[j-1], array[j]
+        else:
+            break
+print(array)
